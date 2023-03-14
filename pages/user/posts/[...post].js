121f1dae1c4e03cc6590005f666d5e22b1f36351
Ahad69 <ahadxx99@gmail.com>
2023-03-14 10:30:07 +0600

    updated

--- pages/user/posts/[...post].js
+++ pages/user/posts/[...post].js
@@ -159,24 +159,6 @@ const Post = () => {
     }
   };
 
-  // const [fileList, setFileList] = useState([]);
-  // const onChange = ({ fileList: newFileList }) => {
-  //   setFileList(newFileList);
-  // };
-  // const onPreview = async (file) => {
-  //   let src = file.url;
-  //   if (!src) {
-  //     src = await new Promise((resolve) => {
-  //       const reader = new FileReader();
-  //       reader.readAsDataURL(file.originFileObj);
-  //       reader.onload = () => resolve(reader.result);
-  //     });
-  //   }
-  //   const image = new Image();
-  //   image.src = src;
-  //   const imgWindow = window.open(src);
-  //   imgWindow?.document.write(image.outerHTML);
-  // };
 
   return (
     <div>
